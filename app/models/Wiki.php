2a2dee0d48119782ee689a749df4fc838b65d31b
walid-lhaila <walidlhaila00@gmail.com>
2024-01-09 22:54:02 +0100

    all classes

--- app/models/Wiki.php
+++ app/models/Wiki.php
@@ -3,7 +3,75 @@ class Wiki {
     private $wikiId;
     private $title;
     private $content;
-    private 
+    private $wikiImg;
+    private AppUser $userId;
+    private Category $categoryId;
+    private $created_at;
+    public function __construct(){
+
+    }
+    public function getWikiId(){
+        return $this->wikiId;
+    }
+
+    public function setWikiId($wikiId){
+        $this->wikiId = $wikiId;
+    }
+
+    // Getter and Setter for title
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function setTitle($title){
+        $this->title = $title;
+    }
+
+    // Getter and Setter for content
+    public function getContent(){
+        return $this->content;
+    }
+
+    public function setContent($content){
+        $this->content = $content;
+    }
+
+    // Getter and Setter for wikiImg
+    public function getWikiImg(){
+        return $this->wikiImg;
+    }
+
+    public function setWikiImg($wikiImg){
+        $this->wikiImg = $wikiImg;
+    }
+
+    // Getter and Setter for userId
+    public function getUserId(){
+        return $this->userId;
+    }
+
+    public function setUserId($userId){
+        $this->userId = $userId;
+    }
+
+    // Getter and Setter for categoryId
+    public function getCategoryId(){
+        return $this->categoryId;
+    }
+
+    public function setCategoryId($categoryId){
+        $this->categoryId = $categoryId;
+    }
+
+    // Getter and Setter for created_at
+    public function getCreatedAt(){
+        return $this->created_at;
+    }
+
+    public function setCreatedAt($created_at){
+        $this->created_at = $created_at;
+    }
+
 }
 
 
