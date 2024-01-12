b1615baae8a24fe76135b18987a8366d4229b472
walid-lhaila <walidlhaila00@gmail.com>
2024-01-12 17:19:33 +0100

    project Done

--- app/services/implementation/WikiServiceImp.php
+++ app/services/implementation/WikiServiceImp.php
@@ -2,7 +2,24 @@
     Class WikiServiceImp implements WikiService{
         private Database $db;
 
-        public function 
+        public function __construct(){
+            $this->db = Database::getInstance();
+        }
+
+        public function addWiki(Wiki $wiki){
+
+        }
+        public function displayWiki(){
+            $fetshWikiQuery = "SELECT * FROM wiki JOIN category ON wiki.categoryId = category.categoryId JOIN appUser On wiki.userId = appUser.userId WHERE wiki.archived_at IS NULL ORDER BY wiki.created_at DESC";
+
+        }
+        public function updateWiki(Wiki $wiki){
+
+        }
+        public function deleteWiki($wikiId){
+
+        }
+        
     }    
 
 ?>
